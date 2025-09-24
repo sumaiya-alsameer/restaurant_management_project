910c857a3e380cc5e7d5d0184bc178fa3a2015e5
sumaiya-alsameer <sumaiya.alsameer@gmail.com>
2025-09-24 11:09:39 +0530

    50159

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/4.2/ref/settings/
+https://docs.djangoproject.com/en/4.2/ref/settings
 """
 
 from pathlib import Path
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken'
     'home',
     'account',
     'products',
